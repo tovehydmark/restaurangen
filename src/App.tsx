686abb95d8013a181b54645c9b464f297db0c097
tovehydmark <tovehydmark@gmail.com>
2022-04-05 09:54:18 +0200

    navbar with routes created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Layout } from "./components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
+import { Booking } from "./components/Booking";
+import { Menu } from "./components/Menu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Booking" element={<Booking />} />
+            <Route path="Menu" element={<Menu />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
