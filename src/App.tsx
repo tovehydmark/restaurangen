6aeae7db5f0834ec6072c309493374894bdd69d3
Frida <friden@hotmail.com>
2022-04-05 16:34:47 +0200

    404 started

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+=======
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Layout } from "./components/Layout";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
+import { Contact } from "./components/Contact";
+import { Booking } from "./components/Booking";
+import { Menu } from "./components/Menu";
+import { NotFound } from "./components/NotFound";
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,6 +33,19 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/Contact" element={<Contact />} />
+            <Route path="/Booking" element={<Booking />} />
+            <Route path="Menu" element={<Menu />} />
+            <Route path="*" element= {<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,8 @@
+export function NotFound() {
+  return <div>
+    <h1>404</h1>
+    <p>Something smells fishy</p>
+    <img src="/images/CrabwBG.png"></img>
+    <p>The page you were looking for was not found</p>
+  </div>;
+}
