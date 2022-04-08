19862a8bfc42f0cecc0982d38dad4c3a2988eb35
Frida <friden@hotmail.com>
2022-04-08 09:41:18 +0200

    Update NotFound.tsx

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,4 +1,19 @@
 import CrabwBG from "../images/CrabwBG.png"
+import styled from "styled-components"
+
+export const StyledHeading = styled.h1`
+color: #CF1D15;
+font-size: 8rem;
+font-family: "Merriweather", serif;   
+`
+export const StyledH3tag = styled.h3`
+color: #004F4A;
+font-size: 3rem;
+font-family: "Merriweather", serif;
+`
+export const StyledP = styled.p`
+font-family: "Roboto", sans-serif;
+font-size: 2rem;`
 
 export function NotFound() {
   return <div>
