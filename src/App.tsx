1e01464802c0abc11c1fa48b7e0c0033e5319901
tovehydmark <tovehydmark@gmail.com>
2022-04-08 09:54:23 +0200

    small changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 import { Layout } from "./components/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
