8aa63d4d6b70794713e6a9aa64df3f0f0cffe1ac
tovehydmark <tovehydmark@gmail.com>
2022-04-09 18:18:32 +0200

    added styling for open nav in mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,15 +10,19 @@ export const Nav = styled.nav`
 
 export const Li = styled.li`
   list-style-type: none;
-  padding-left: 10px;
+  /* padding-left: 10px; */
+  Link {
+    display: flex;
+    flex-direction: row;
+  }
 `;
 
 const HamburgerContainer = styled.div`
   display: none;
   @media (max-width: 767px) {
     display: fixed;
-    top: 0;
-    right: 0;
+    /* top: 0; //Lägger sig fortfarande till vänster, vet ej varför
+    right: 0; */
     z-index: 10;
   }
 `;
@@ -30,7 +34,6 @@ export function Header() {
 
   function toggleHamburger() {
     setHamburgerOpen(!hamburgerOpen);
-    console.log(hamburgerOpen);
   }
 
   //Styled components bör inte ligga inuti en komponent såhär, får upp varningar. Lät den ligga för tillfället eftersom jag behövde toggle-funktionen för display och den hittades inte om jag la Ul utanför
@@ -38,29 +41,52 @@ export function Header() {
   const Ul = styled.ul`
     display: flex;
     flex-wrap: wrap;
-    float: right;
     margin: 20px 0;
     padding: 0 25px;
-
     display: ${hamburgerOpen ? "inline" : "none"};
 
     @media (max-width: 767px) {
-      background-color: lightblue;
-      height: 100vh;
-      z-index: 100;
-      width: 70vw;
-      /* margin-top: 50px; */
       position: absolute;
+      right: 0;
+      z-index: 10;
+      height: 100vh;
+      width: 100%;
+      margin-top: 30px;
+      background-color: rgba(0, 79, 74, 0.9);
+      font-size: 2rem;
+      list-style: none;
+
+      li {
+        margin: 30px;
+      }
+
+      a {
+        color: white;
+        text-decoration: none;
+
+        :hover {
+          text-decoration: underline;
+        }
+      }
     }
   `;
   return (
     <header>
       <Nav className="navigation">
-        <Ul>
-          <Li>
+        <Ul onClick={toggleHamburger}>
+          <li>
+            {" "}
             <Link to="/">The Codfather</Link>
+          </li>
+
+          <li>
             <Link to="/Contact">Kontakt</Link>
+          </li>
+
+          <li>
             <Link to="/Booking">Boka bord</Link>
+          </li>
+          <Li>
             <Link to="Menu">Meny</Link>
           </Li>
         </Ul>
