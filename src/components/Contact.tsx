e5c1132eca50243535dd65e1efc8a34fb161fec7
John Eliasson <85025404+JohnEliasson@users.noreply.github.com>
2022-04-12 08:51:01 +0200

    Fixat bas för footer och Kontakt

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,3 +1,56 @@
+import "../style/style.scss";
+import axios from "axios";
+
 export function Contact() {
-  return <></>;
+  return (
+    <section>
+    
+    <h2>Kontakt</h2>
+    <article>
+    <p>
+      Laxgatan 14
+      <br />
+      135 79
+      <br />
+      Torskhamn
+    </p>
+    </article>
+    <h4>Öppettider:</h4>
+    <article>
+      <p>
+        Mån-Tors: 11.00 - 22.00
+        <br />
+        Fre-Sön: 11.00 - 23.00
+      </p>
+    </article>
+
+    <footer>
+
+    </footer>
+  </section>
+   
+  );
 }
+/*
+    <section>
+    
+    <h2>Kontakt</h2>
+    <article>
+    <p>
+      Laxgatan 14
+      <br />
+      135 79
+      <br />
+      Torskhamn
+    </p>
+    </article>
+    <h4>Öppettider:</h4>
+    <article>
+      <p>
+        Mån-Tors: 11.00 - 22.00
+        <br />
+        Fre-Sön: 11.00 - 23.00
+      </p>
+    </article>
+  </section>
+  */
