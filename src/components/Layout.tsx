19b85708d6493ef893c51c3ce67cc5e47ac0ad08
John Eliasson <85025404+JohnEliasson@users.noreply.github.com>
2022-04-12 08:52:41 +0200

    Fixat bas för footer samt kontakt

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,19 @@ export function Layout() {
       <main>
         <Outlet></Outlet>
       </main>
-      <footer></footer>
+      <footer>
+      <p>
+       Öppettider:
+       <br />
+       Mån-Tors: 11.00 - 22.00
+        <br />
+        Fre-Sön: 11.00 - 23.00
+     </p>
+
+     <a href="#">Facebook</a>
+     <a href="#">Instagram</a>
+
+      </footer>
     </>
   );
 }
