a3eef0db873ac6bff0c7adc404e7989f761bc42b
tovehydmark <tovehydmark@gmail.com>
2022-04-14 11:43:34 +0200

    created styled component

--- src/components/styledComponents/Button.tsx
+++ src/components/styledComponents/Button.tsx
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Button = styled.button``;
+export const Button = styled.button`
+  background-color: #008178;
+  color: #fff;
+  border-radius: 8px;
+  border: none;
+  font-size: 1rem;
+  font-family: "Roboto", sans-serif;
+  cursor: pointer;
+  width: 80px;
+`;
