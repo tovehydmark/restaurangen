357ef2ce655113d917561141c337e6e099e4a53e
tovehydmark <tovehydmark@gmail.com>
2022-04-22 13:19:45 +0200

    style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
 import "../style/style.scss";
-import { HamburgerDiv } from "./styledComponents/HamburgerDiv";
-import { UnorderedList } from "./styledComponents/Ul";
+import { Hamburger } from "./Hamburger";
+import { HamburgerContainer } from "./styledComponents/HamburgerContainer";
+import { UnorderedList } from "./styledComponents/UnorderedList";
+
+//Boolean hamburgerOpen is used to toggle hamburger menu on click
 
 export function Header() {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
@@ -12,68 +14,24 @@ export function Header() {
     setHamburgerOpen(!hamburgerOpen);
   }
 
-  //STYLED COMPONENTS//
-  //Styled Nav used for navigation. In mobile view, the Nav appear as a hamburger menu.
-  const Nav = styled.nav`
-    width: 100%;
-    height: 70px;
-
-    @media (max-width: 1289px) {
-      width: 40px;
-      position: absolute;
-      right: 10px;
-      align-items: flex-end;
-    }
-
-    p {
-      display: ${!hamburgerOpen ? "inline" : "none"};
-      margin: 0;
-      position: fixed;
-      left: 16px;
-      font-size: 1.5rem;
-      top: 17px;
-
-      @media (min-width: 1290px) {
-        display: none;
-      }
-    }
-    //Active link shows underline, changed from Link to NavLink for boolean isActive
-    .link-active {
-      text-decoration: underline black solid 2px;
-    }
-  `;
-
-  //Kanske flytta denna styling också
-  const HamburgerContainer = styled.div`
-    display: none;
-    @media (max-width: 1289px) {
-      width: 40px;
-      display: fixed;
-      display: flex;
-      flex-wrap: wrap;
-      margin: 16px 0 0 8px;
-      top: 0;
-      right: 0;
-      z-index: 10;
-    }
-  `;
-
   return (
     <header>
-      <Nav className="navigation">
+      <nav className="navigation">
         <UnorderedList
           isOpen={hamburgerOpen}
           toggleHamburger={toggleHamburger}
         ></UnorderedList>
-        <p>
+
+        <p className={`${hamburgerOpen ? "hide" : "show"}`}>
           <NavLink className="mobileHomeLogo" to="/">
             The Codfather
           </NavLink>
         </p>
+
         <HamburgerContainer onClick={toggleHamburger}>
-          <HamburgerDiv isOpen={hamburgerOpen}></HamburgerDiv>
+          <Hamburger isOpen={hamburgerOpen}></Hamburger>
         </HamburgerContainer>
-      </Nav>
+      </nav>
     </header>
   );
 }
